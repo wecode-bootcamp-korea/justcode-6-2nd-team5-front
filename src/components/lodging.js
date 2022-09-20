b7b9d4540490617d2110b587b08f68b3c6fd72b1
lkw1996 <lkw3703@gmail.com>
2022-09-20 14:01:14 +0900

    [Add] fetch mock data and gets stored in state

--- src/components/lodging.js
+++ src/components/lodging.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Lodging.scss";
 
 function Lodging() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/lodging.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.lodgeInfo);
+      });
+  }, []);
+
   return <div>Lodging</div>;
 }
 
