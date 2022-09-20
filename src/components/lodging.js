5447c6d51ab9a8bd5f87fcd7e381a4c2ede12f5d
lkw1996 <lkw3703@gmail.com>
2022-09-20 14:11:58 +0900

    [Add] general layout of the component using swiper and map

--- src/components/lodging.js
+++ src/components/lodging.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination } from "swiper";
 
 import "./Lodging.scss";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+SwiperCore.use([Navigation, Pagination]);
 
 function Lodging() {
   const [data, setData] = useState([]);
@@ -13,7 +20,33 @@ function Lodging() {
       });
   }, []);
 
-  return <div>Lodging</div>;
+  return (
+    <div className="content-wrapper">
+      <h1 className="title">제주다운 감성숙소 추천</h1>
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {data.map((data) => (
+          <SwiperSlide key={data.id}>
+            <div className="lodge-wrapper">
+              <div>
+                <img src={data.img} alt="image" />
+              </div>
+              <div className="lodge-info-wrapper">
+                <span>{data.title}</span>
+                <span>{data.tag}</span>
+
+                <span>{data.location}</span>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
 export default Lodging;
