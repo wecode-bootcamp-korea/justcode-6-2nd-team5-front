57f0756e4031a03904a12db53644354f2bca3fe0
Chooteacher <zmz14s@gmail.com>
2022-09-20 22:46:53 +0900

    [Add]: 로그인 API

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import "./login.scss";
 import naverIcon from "../../assets/images/btnN.png";
 import kakaoIcon from "../../assets/images/btnK.png";
 // import loginBanner from "../../assets/images/loginBr.png";
 
 function Login() {
+  const [userEmail, setUserEamil] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const userEmailHandler = (e) => {
+    const userEmailValue = e.target.value;
+    setUserEamil(userEmailValue);
+  };
+
+  const userPasswordHandler = (e) => {
+    const userPasswordValue = e.target.value;
+    setUserPassword(userPasswordValue);
+  };
+
+  const loginClick = () => {
+    fetch("http://localhost:8000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: userEmail,
+        password: userPassword,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.token) {
+          localStorage.setItem("token", result.token);
+          alert("로그인 성공");
+          window.location.replace("/");
+        } else {
+          alert("로그인 실패");
+        }
+      });
+  };
+
   return (
     <div className="login">
       <div className="login-container">
@@ -29,11 +66,20 @@ function Login() {
           <form className="tab-form">
             <div className="tab-input">
               <label htmlFor="id">아이디(이메일)</label>
-              <input id="id" placeholder="E-mail을 입력해주세요."></input>
+              <input
+                id="id"
+                placeholder="E-mail을 입력해주세요."
+                onChange={userEmailHandler}
+              ></input>
             </div>
             <div className="tab-input">
               <label htmlFor="pw">비밀번호</label>
-              <input id="pw" placeholder="비밀번호를 입력해주세요."></input>
+              <input
+                type="password"
+                id="pw"
+                placeholder="비밀번호를 입력해주세요."
+                onChange={userPasswordHandler}
+              ></input>
             </div>
           </form>
           <div className="login-chks">
@@ -47,7 +93,9 @@ function Login() {
             </span>
           </div>
           <div className="btn-area">
-            <button className="login-action-btn">로그인</button>
+            <button className="login-action-btn" onClick={loginClick}>
+              로그인
+            </button>
           </div>
           <div className="login-btn">
             <a className="btn-aft">아이디 찾기</a>
