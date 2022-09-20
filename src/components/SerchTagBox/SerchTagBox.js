3600a10a45f2fbef43638a2cf14209382c28bc8b
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-09-21 03:22:38 +0900

    [Add] 빠른 검색 박스 태그 선택시 UI 변경 기능 추가

--- src/components/SerchTagBox/SerchTagBox.js
+++ src/components/SerchTagBox/SerchTagBox.js
@@ -2,20 +2,9 @@ import { useState } from "react";
 import "./SerchTagBox.scss";
 
 function SerchTagBox() {
-  const tagList = [
-    "캐스퍼",
-    "애견동반",
-    "낚시용품 가능",
-    "오픈카",
-    "주유비 SAVE, 전기차",
-    "ALL NEW 신차",
-    "전기충전",
-    "BMW",
-    "차박/캠핑카",
-  ];
-
   // 버튼 슬라이드
   const [tagListSlide, setTagListSlide] = useState(0);
+  // 사용자의 페이지 너비와 버튼 슬라이드의 움직임값은 반비례 관계
   const pageWidth = document.documentElement.scrollWidth;
   const slideCondition = 1000000 / pageWidth;
 
@@ -26,6 +15,73 @@ function SerchTagBox() {
   // prev 버튼이 눌렸을 때 (next 버튼이 한번이라도 눌렸을 때만 동작하도록 함)
   const prevSlide = () => {
     if (tagListSlide < 0) setTagListSlide((prev) => prev + 300);
+  };
+
+  // 선택된 태그 state value
+  const [isOnTag0, setIsOnTag0] = useState(false);
+  const [isOnTag1, setIsOnTag1] = useState(false);
+  const [isOnTag2, setIsOnTag2] = useState(false);
+  const [isOnTag3, setIsOnTag3] = useState(false);
+  const [isOnTag4, setIsOnTag4] = useState(false);
+  const [isOnTag5, setIsOnTag5] = useState(false);
+  const [isOnTag6, setIsOnTag6] = useState(false);
+  const [isOnTag7, setIsOnTag7] = useState(false);
+  const [isOnTag8, setIsOnTag8] = useState(false);
+
+  // 태그 mockdata
+  const tagList = [
+    {
+      tagName: "캐스퍼",
+      disabled: isOnTag0,
+    },
+    {
+      tagName: "애견동반",
+      disabled: isOnTag1,
+    },
+    {
+      tagName: "낚시용품 가능",
+      disabled: isOnTag2,
+    },
+    {
+      tagName: "오픈카",
+      disabled: isOnTag3,
+    },
+    {
+      tagName: "주유비 SAVE, 전기차",
+      disabled: isOnTag4,
+    },
+    {
+      tagName: "ALL NEW 신차",
+      disabled: isOnTag5,
+    },
+    {
+      tagName: "전기충전",
+      disabled: isOnTag6,
+    },
+    {
+      tagName: "BMW",
+      disabled: isOnTag7,
+    },
+    {
+      tagName: "차박/캠핑카",
+      disabled: isOnTag8,
+    },
+  ];
+
+  // 태그 선택/선택해제 함수
+  const tagSelect = (e) => {
+    console.log(e.target.id);
+    const tagId = e.target.id;
+
+    if (tagId === "0") setIsOnTag0((prev) => !prev);
+    if (tagId === "1") setIsOnTag1((prev) => !prev);
+    if (tagId === "2") setIsOnTag2((prev) => !prev);
+    if (tagId === "3") setIsOnTag3((prev) => !prev);
+    if (tagId === "4") setIsOnTag4((prev) => !prev);
+    if (tagId === "5") setIsOnTag5((prev) => !prev);
+    if (tagId === "6") setIsOnTag6((prev) => !prev);
+    if (tagId === "7") setIsOnTag7((prev) => !prev);
+    if (tagId === "8") setIsOnTag8((prev) => !prev);
   };
 
   return (
@@ -39,12 +95,21 @@ function SerchTagBox() {
       )}
       <div className="tag-list-wrap">
         <div className="tag-list" style={{ marginLeft: `${tagListSlide}px` }}>
-          {tagList.map((tag, index) => {
-            return <button key={index}>#{tag}</button>;
+          {tagList.map((tagInfo, index) => {
+            return (
+              <button
+                className={tagInfo.disabled ? "tag-on" : "tag-off"}
+                key={index}
+                id={index}
+                onClick={tagSelect}
+              >
+                {tagInfo.disabled && <span>✓</span>}#{tagInfo.tagName}
+              </button>
+            );
           })}
         </div>
       </div>
-      {/* next 버튼 3번 누르면 사라지도록 함 */}
+      {/* 사용자가 설정한 페이지 너비가 넓어질수록 slideCondition는 작아짐*/}
       {tagListSlide > -slideCondition && (
         <div className="next-btn" onClick={nextSlide}>
           &gt;
