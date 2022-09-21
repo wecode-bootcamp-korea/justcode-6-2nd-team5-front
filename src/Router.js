f20aa6b140ab6ab002e2cd9210e900e44eda51d5
lkw1996 <lkw3703@gmail.com>
2022-09-21 17:11:54 +0900

    [Edit] common 컴포넌트 메인으로 불러오는 작업

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Mypage from "./pages/Mypage/Mypage";
 import RentCar from "./pages/RentCar/RentCar";
-import Lodge from "./components/Lodge/Lodge";
-import Banner from "./components/Banner/Banner";
-import Car from "./components/Car/Car";
-import Restaurant from "./components/Restaurant/Restaurant";
-import Promotion from "./components/Promotion/Promotion";
+import Main from "./pages/Main/Main";
 
 function Router() {
   return (
@@ -15,11 +11,7 @@ function Router() {
       <Routes>
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/rentcar" element={<RentCar />} />
-        <Route path="/Lodge" element={<Lodge />} />
-        <Route path="/Banner" element={<Banner />} />
-        <Route path="/Car" element={<Car />} />
-        <Route path="/Restaurant" element={<Restaurant />} />
-        <Route path="/Promotion" element={<Promotion />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
