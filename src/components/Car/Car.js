28273164d61a584279a0bcae180640d6bf530003
lkw1996 <lkw3703@gmail.com>
2022-09-21 16:22:06 +0900

    [Style] Car 컴포넌트 스타일링 추가

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination } from "swiper";
+import { FaCar } from "react-icons/fa";
 
+import "./Car.scss";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -20,7 +22,10 @@ function Car() {
 
   return (
     <div className="content-wrapper">
-      <h1 className="title">최저가 렌터카 추천</h1>
+      <h1 className="title">
+        최저가 렌터카 추천
+        <FaCar />
+      </h1>
       <Swiper
         spaceBetween={20}
         slidesPerView={3}
