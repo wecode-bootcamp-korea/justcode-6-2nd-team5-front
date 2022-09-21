2303541c4881c9958777277048e8ef9dc2346657
lkw1996 <lkw3703@gmail.com>
2022-09-21 16:01:53 +0900

    [Add] 메인 Banner 레이아웃 및 기능 추가

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mypage from "./pages/Mypage/Mypage";
 import RentCar from "./pages/RentCar/RentCar";
 import Lodge from "./components/Lodge/Lodge";
+import Banner from "./components/Banner/Banner";
 
 function Router() {
   return (
@@ -12,6 +13,7 @@ function Router() {
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/rentcar" element={<RentCar />} />
         <Route path="/Lodge" element={<Lodge />} />
+        <Route path="/Banner" element={<Banner />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination, Autoplay } from "swiper";
+
+// import "./Banner.scss";
+import "swiper/css";
+import "swiper/css/pagination";
+
+SwiperCore.use([Pagination, Autoplay]);
+
+function Banner() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/bannerInfo.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.bannerInfo);
+      });
+  }, []);
+
+  return (
+    <div className="content-wrapper">
+      <Swiper slidesPerView={1} autoplay pagination={{ clickable: false }}>
+        {data.map((data) => (
+          <SwiperSlide key={data.id}>
+            <div className="banner-info-wrapper">
+              <div className="banner-img">
+                <img src={data.img} alt="image" />
+              </div>
+              <div className="banner-info">
+                <p>{data.info1}</p>
+                <p>{data.info2}</p>
+                <p>{data.info3}</p>
+                <p>{data.info4}</p>
+                <p>{data.info5}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Banner;
