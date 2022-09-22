9e51516475aeb80c632d1eef5486aa020d29d694
Lee Eun Ji <105857105+Chooteacher@users.noreply.github.com>
2022-09-22 16:50:40 +0900

    [Add]: 로그인 레이아웃, 기능 구현 (#5)

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mypage from "./pages/Mypage/Mypage";
 import RentCar from "./pages/RentCar/RentCar";
 import Main from "./pages/Main/Main";
+import Login from "./pages/login/login";
 
 function Router() {
   return (
@@ -12,6 +13,7 @@ function Router() {
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/rentcar" element={<RentCar />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
