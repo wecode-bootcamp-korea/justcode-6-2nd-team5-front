b7130e562308942ae51ee76bf31296bede0d24ca
Lee Eun Ji <105857105+Chooteacher@users.noreply.github.com>
2022-09-25 13:26:46 +0900

    [Add]: 회원가입부분 레이아웃, API, 유효성 검사 (#6)

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,9 @@ import Mypage from "./pages/Mypage/Mypage";
 import RentCar from "./pages/RentCar/RentCar";
 import Main from "./pages/Main/Main";
 import Login from "./pages/login/login";
+import TermsSignup from "./components/signup/termsSignup";
+import Signup from "./pages/signup/signup";
+import SignupFin from "./components/signup/signupFin";
 
 function Router() {
   return (
@@ -14,6 +17,9 @@ function Router() {
         <Route path="/rentcar" element={<RentCar />} />
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/termsSignup" element={<TermsSignup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signupFin" element={<SignupFin />} />
       </Routes>
     </BrowserRouter>
   );
